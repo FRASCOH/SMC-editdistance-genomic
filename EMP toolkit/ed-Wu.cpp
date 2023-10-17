55056ee3a367d6d700da791f405456dd65c61007
FRASCOH <45537127+FRASCOH@users.noreply.github.com>
2023-10-17 10:41:57 +0200

    Ricorsività totale ed-Wu.cpp
    Recursive

--- EMP toolkit/ed-Wu.cpp
+++ EMP toolkit/ed-Wu.cpp
@@ -13,20 +13,63 @@ Integer max(Integer a, Integer b) {
 }
 
 
-int snake( vector<int> sequenceAlice, vector<int> sequenceBob, int s1len, int s2len, int k, int j) {
+Integer snake( vector<int> sequenceAlice, vector<int> sequenceBob, int s1len, int s2len, int k, int j) {
     Integer i_emp(intBitSize, j - k, ALICE);
     Integer j_emp(intBitSize, j, ALICE);
     Integer one(intBitSize, 1, ALICE);
     Integer zero(intBitSize, 0, ALICE);
     
     if (j >= s2len || k >= s1len) {
-        return j;
+        return j_emp;
     }
     
     Integer result = j_emp.select((sequenceAlice[k] != sequenceBob[j]), zero);
-    if(result.reveal<int>() != 0) return j;
+    if(result.reveal<int>() != 0) return j_emp;
 
     return snake(s1, s2, s1len, s2len, k + 1, j + 1);
+}
+
+void snake_recursive_wrapper(int* ptr_fp, const char* sequenceAlice, const char* sequenceBob, int lengthAlice, int lengthBob, int intBitSize, int delta, int offset, int& p) {
+    snake_recursive(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, -p, offset);
+    snake_recursive_reverse(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, delta + p, offset, delta, p);
+
+    int newValue = snake(sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, delta, std::max(*(ptr_fp + delta - 1 + offset) + 1, *(ptr_fp + delta + 1 + offset)));
+    if (*(ptr_fp + delta + offset) != newValue) {
+        *(ptr_fp + delta + offset) = newValue;
+        p = p + 1;
+        snake_recursive_wrapper(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, delta, offset, p);
+    }
+}
+
+int snake_recursive(int* ptr_fp, const char* sequenceAlice, const char* sequenceBob, int lengthAlice, int lengthBob, int intBitSize, int k, int offset) {
+    if (k > delta - 1) {
+        return 0;
+    }
+
+    int previous = (k > -p) ? *(ptr_fp + k - 1 + offset) + 1 : 0;
+    int next = *(ptr_fp + k + 1 + offset);
+
+    int result = snake_recursive(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k + 1, offset) + 1;
+    result = std::max(result, snake_recursive(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k - 1, offset) + 1);
+    result = std::max(result, snake_recursive(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k + 1, offset));
+
+    *(ptr_fp + k + offset) = result;
+    return result;
+}
+
+void snake_recursive_reverse(int* ptr_fp, const char* sequenceAlice, const char* sequenceBob, int lengthAlice, int lengthBob, int intBitSize, int k, int offset, int delta, int p) {
+    if (k < delta + 1) {
+        return;
+    }
+
+    int previous = (k < delta + p) ? *(ptr_fp + k + 1 + offset) + 1 : 0;
+    int next = *(ptr_fp + k - 1 + offset);
+
+    int result = snake_recursive_reverse(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k - 1, offset, delta, p) + 1;
+    result = std::max(result, snake_recursive_reverse(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k + 1, offset, delta, p) + 1);
+    result = std::max(result, snake_recursive_reverse(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k - 1, offset, delta, p));
+
+    *(ptr_fp + k + offset) = result;
 }
 
 int edit_distance_wu(vector<int> sequence, int lengthAlice, int lengthBob, int intBitSize) {
@@ -44,8 +87,7 @@ int edit_distance_wu(vector<int> sequence, int lengthAlice, int lengthBob, int i
     vector<Integer> fp (lengthAlice + lengthBob + 3);
     fill(&fp[0], &fp[lengthAlice + lengthBob + 3], one_neg);
     Integer* ptr_fp = &fp[0];
-
-    //Integer delta_emp(intBitSize, delta, ALICE);
+    
     Integer p(intBitSize, -1, ALICE);
     
     for (x = 0; x < lengthAlice; x++)
@@ -54,18 +96,26 @@ int edit_distance_wu(vector<int> sequence, int lengthAlice, int lengthBob, int i
     for (x = 0; x < lengthBob; x++)
         sequenceBob.push_back(Integer (intBitSize, sequence[x], BOB));
 
-     do {
+
+
+   snake_recursive_wrapper(ptr_fp, sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, delta, offset, p);
+
+    
+    /* 
+    do {
         p = p + one;
         for (k = -p; k <= delta-1; k++) {
             *(ptr_fp + k + offset) = snake(sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k, max( *(ptr_fp + k - 1 + offset) + 1, *(ptr_fp + k + 1 + offset));
-        }
+        }         
         for (k = delta+p; k >= delta+1; k--) {
             *(ptr_fp + k + offset) = snake(sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k, max( *(ptr_fp + k - 1 + offset) + 1, *(ptr_fp + k + 1 + offset)));
-        }
+        }         
         *(ptr_fp + delta + offset) = snake(sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, delta, max( *(ptr_fp + delta - 1 + offset) + 1, *(ptr_fp + delta + 1 + offset)));
     } while (*(ptr_fp + delta + offset) != lengthBob);
+    */
     
-    return (delta + 2 * p).reveal<int>();
+    Integer delta_emp(intBitSize, delta, ALICE);
+    return (delta_emp + 2 * p).reveal<int>();
     
 }
 
