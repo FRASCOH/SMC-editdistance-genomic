f4694b2ac89a33ca62d2747a00710a9ad0f96f44
FRASCOH <45537127+FRASCOH@users.noreply.github.com>
2023-10-18 22:32:50 +0200

    Update ed-Wu.cpp

--- EMP toolkit/ed-Wu.cpp
+++ EMP toolkit/ed-Wu.cpp
@@ -14,15 +14,16 @@ Integer maximum(Integer a, Integer b) {
 
 
 Integer snake( vector<Integer> sequenceAlice, vector<Integer> sequenceBob, int s1len, int s2len, int intBitSize, int k, Integer j) {
+    cout<< "snake 1"<<endl;
     Integer i_emp(intBitSize, j.reveal<int>() - k, ALICE);
     int j_int = j.reveal<int>();
     Integer one(intBitSize, 1, ALICE);
     Integer zero(intBitSize, 0, ALICE);
-    
+    cout<< "snake 2"<<endl;
     if (j_int >= s2len || k >= s1len) {
         return j;
     }
-    
+    cout<< "snake 3"<<endl;
     Integer result = j.select((sequenceAlice[k] != sequenceBob[j_int]), zero);
     if(result.reveal<int>() != 0) return j;
 
@@ -36,19 +37,20 @@ int edit_distance_wu(vector<int> sequence, int lengthAlice, int lengthBob, int i
     vector<Integer> row2 (lengthBob+1);
     Integer zero(intBitSize, 0, ALICE);
     Integer one(intBitSize, 1, ALICE);
-    int x, k;
+    Integer one_neg(32, -1, PUBLIC);
+    int x, k=0;
     int p = -1;
     int delta = lengthBob - lengthAlice;
     int offset = lengthAlice + 1;
     vector<Integer> fp (lengthAlice + lengthBob + 3);
     
-    for (x = 0; x < lengthAlice + lengthBob + 3; x++)
-        fp.push_back(Integer (intBitSize, -1, ALICE));
-        
+    for (x = 0; x < (lengthAlice + lengthBob + 3); x++)
+        fp[x] = one_neg;
+    
     //fill(&fp[0], &fp[lengthAlice + lengthBob + 3], -1);
    
     Integer* ptr_fp = &fp[0];
-
+    
     for (x = 0; x < lengthAlice; x++)
         sequenceAlice.push_back(Integer (intBitSize, sequence[x], ALICE));
 
@@ -57,20 +59,32 @@ int edit_distance_wu(vector<int> sequence, int lengthAlice, int lengthBob, int i
 
      Integer lengthBob_emp(32, lengthBob, PUBLIC);
      
-     int ptr_int;
      
+        int ptr_int= 0; 
+     
+     cout<<"Inizio do while"<<endl;
+     
+    
      do {
         p++;
+        cout<<"Dentro do while"<<endl;
+        cout<<"k: " << k << " p: "<<p<< " delta: "<<delta<<endl;
         for (k = -p; k <= delta-1; k++) {
+        cout<< "Primo for: "<< k <<endl;
             *(ptr_fp + k + offset) = snake(sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k, maximum( *(ptr_fp + k - 1+ offset) + one, *(ptr_fp + k + 1 + offset)));
+            
         }
         for (k = delta+p; k >= delta+1; k--) {
+        cout<< "Secondo for: "<< k <<endl;
             *(ptr_fp + k + offset) = snake(sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, k, maximum( *(ptr_fp + k - 1 + offset) + one, *(ptr_fp + k + 1 + offset)));
         }
-        *(ptr_fp + delta + offset) = snake(sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, delta, maximum( *(ptr_fp + delta - 1 + offset) + one, *(ptr_fp + delta + 1 + offset)));
+        cout<< "Pre fine for" <<endl;
+        //cout<<"k: " << k << " p: "<<p<< " delta: "<<delta<<endl;
+       // *(ptr_fp + delta + offset) 
+        fp[delta+offset] = snake(sequenceAlice, sequenceBob, lengthAlice, lengthBob, intBitSize, delta, maximum( fp[delta-1+offset] + one, fp[delta + 1 + offset]));
         
        ptr_int = (*(ptr_fp + delta + offset)).reveal<int>();
-        
+        cout<< "Pre fine for, PTR_INT: " <<ptr_int<<endl;
     } while (ptr_int != lengthBob);
     
     
